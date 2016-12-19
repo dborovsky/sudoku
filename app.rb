884e75bb069dd07c9a4a31d19b41c1d3f0e9356b
Dmitriy Borowskiy <d.borowskij@gmail.com>
2016-12-19 13:12:07 +0300

    mail

--- app.rb
+++ app.rb
@@ -159,7 +159,11 @@ post '/restore' do
       :from => 'xtrance1991@gmail.com',
       :subject => 'Restored password: Sudoku',
       :body => new_password,
-      :via => :sendmail
+      :via => :sendmail,
+      :via_options => {
+        :port => '25',
+        :domain => 'sudoku-spielen.org'
+      }
     })
     user.update_attribute(:password, new_password)
 
@@ -175,7 +179,6 @@ post '/restore' do
   end
 end
 
-
 post '/send' do
   Pony.mail({
     :to => 'xtrance1991@gmail.com',
@@ -187,12 +190,12 @@ post '/send' do
     #   :address              => 'smtp.gmail.com',
     #   :port                 => '587',
     #   :enable_starttls_auto => true,
-    #   :user_name            => '',
-    #   :password             => 'dzmitry1982',
-    #   #:authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
-    #   :domain               => "sudoku11.herokuapp.com" # the HELO domain provided by the client to the server
-    # }
-  })
+    #   :user_name            => 'd.borowskij',
+    #   :password             => '',
+    #   :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+    #   :domain               => "localhost" # the HELO domain provided by the client to the server
+    }
+  )
 
   content_type :json
   { 
